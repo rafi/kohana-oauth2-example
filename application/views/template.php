7406e40b3b4085d2d01da4d22738dfec04c87fdb
Kiall Mac Innes <kiall@managedit.ie>
2011-08-23 11:53:09 +0100

    Tiny bit of CSS to make it easier to follow..

--- application/views/template.php
+++ application/views/template.php
@@ -3,6 +3,15 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<title>Kohana OAuth2 Example - <?=$title ?></title>
+		<style type="text/css">
+			label, input {
+				display: block;
+			}
+
+			input {
+				margin-top: 5px;
+			}
+		</style>
 	</head>
 	<body>
 		<h1>Kohana OAuth2 Example - <?=$title ?></h1>
